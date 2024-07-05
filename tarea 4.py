ff6fe1376c5fbd9d41e65451e157763d87ce34f2
tomasdarracq <juan.darracq@correo.ucu.edu.uy>
2024-07-05 16:04:39 -0300

    fix

--- tarea 4.py
+++ tarea 4.py
@@ -5,43 +5,36 @@ datos = pd.read_csv('titanik.csv')
 datos.head()
 
 
+
 ################################################################### parte 2
-media_edad_femenina = datos[datos['sex'] == 'female']['age'].mean()
-media_edad_masculina = datos[datos['sex'] == 'male']['age'].mean()
-datos.loc[(datos['age'].isnull()) & (datos['sex'] == 'female'), 'age'] = media_edad_femenina
-datos.loc[(datos['age'].isnull()) & (datos['sex'] == 'male'), 'age'] = media_edad_masculina
+media_edad_femenina = datos[datos['gender'] == 'female']['age'].mean()
+media_edad_masculina = datos[datos['gender'] == 'male']['age'].mean()
+datos.loc[(datos['age'].isnull()) & (datos['gender'] == 'female'), 'age'] = media_edad_femenina
+datos.loc[(datos['age'].isnull()) & (datos['gender'] == 'male'), 'age'] = media_edad_masculina
 
 ############################################## parte 3 
 promedio_edad = datos['age'].mean()
-
 mediana_edad = datos['age'].median()
-
 moda_edad = datos['age'].mode()[0]
-
 rango_edad = datos['age'].max() - datos['age'].min()
-
 varianza_edad = datos['age'].var()
-
 desviacion_edad = datos['age'].std()
 
-promedio_edad, mediana_edad, moda_edad, rango_edad, varianza_edad, desviacion_edad
+print(promedio_edad, mediana_edad, moda_edad, rango_edad, varianza_edad, desviacion_edad)
 
 ###################################################### parte 4
 tasa_supervivencia_general = datos['survived'].mean()
-tasa_supervivencia_general
+print(tasa_supervivencia_general)
 
 ########################################################parte 5 
-
-tasa_supervivencia_femenina = datos[datos['sex'] == 'female']['survived'].mean()
-tasa_supervivencia_masculina = datos[datos['sex'] == 'male']['survived'].mean()
-
-tasa_supervivencia_femenina, tasa_supervivencia_masculina
+tasa_supervivencia_femenina = datos[datos['gender'] == 'female']['survived'].mean()
+tasa_supervivencia_masculina = datos[datos['gender'] == 'male']['survived'].mean()
+print(tasa_supervivencia_femenina, tasa_supervivencia_masculina)
 
 ############################################################## parte 6 
-
-plt.hist(datos[datos['pclass'] == 1]['age'], bins=15, alpha=0.6, label='Primera Clase')
-plt.hist(datos[datos['pclass'] == 2]['age'], bins=15, alpha=0.6, label='Segunda Clase')
-plt.hist(datos[datos['pclass'] == 3]['age'], bins=15, alpha=0.6, label='Tercera Clase')
+plt.hist(datos[datos['p_class'] == 1]['age'], bins=15, alpha=0.6, label='Primera Clase')
+plt.hist(datos[datos['p_class'] == 2]['age'], bins=15, alpha=0.6, label='Segunda Clase')
+plt.hist(datos[datos['p_class'] == 3]['age'], bins=15, alpha=0.6, label='Tercera Clase')
 
 plt.legend()
 plt.title('Distribución de Edades por Clase')
@@ -49,8 +42,7 @@ plt.xlabel('Edad')
 plt.ylabel('Frecuencia')
 plt.show()
 
-############################################################# parte7 
-
+############################################################# parte 7 
 datos.boxplot(column='age', by='survived', grid=False)
 plt.title('Distribución de Edades por Supervivencia')
 plt.suptitle('')
